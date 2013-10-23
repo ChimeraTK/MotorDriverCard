2e6d6fece74d37b262841c3b42fb89707d992b7b
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2013-10-23 14:06:06 +0000

    MotorDriverCard compiles. Now the tests can start.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/trunk/src@131 5f601a5c-88b9-47f3-b851-818f6741d00d

--- MotorDriverCardImpl.cc
+++ MotorDriverCardImpl.cc
@@ -4,19 +4,23 @@
 
 namespace mtca4u{
 
-  MotorDriverCardImpl::MotorDriverCardImpl(boost::shared_ptr<MtcaMappedDevice> & mappedDevice,
-				   MotorDriverConfiguration & motorDriverConfiguration){
-    // Fill the register constants from the mappedDevice
-    SPIControlWriteAddress = mappedDevice.getAddress(SPIControlWriteAddressString);
-    SPIControlReadbackAddress = mappedDevice.getAddress(SPIControlReadbackAddressString);
-    
-    // send the configuration
-    
+  MotorDriverCardImpl::MotorDriverCardImpl(boost::shared_ptr< devMap<devBase> > & mappedDevice,
+				   MotorDriverConfiguration & motorDriverConfiguration)
+    : _mappedDevice(mappedDevice),
+      _spiControlWriteRegister( 
+           mappedDevice->getRegObject( SPI_CONTROL_WRITE_ADDRESS_STRING )) ,
+      _spiControlReadbackRegister(
+	   mappedDevice->getRegObject( SPI_CONTROL_READBACK_ADDRESS_STRING )),
+      _powerMonitor(new PowerMonitor)
+  {
     
   }
 
   MotorControler & MotorDriverCardImpl::getMotorControler(unsigned int motorControlerID){
-    if (motorControlerID > 2) throw MotorDriverException("motorControlerID too large");
+    if (motorControlerID > 2){
+      throw MotorDriverException("motorControlerID too large", 
+				 MotorDriverException::WRONG_MOTOR_ID);
+    }
     
     return *(_motorControlers[motorControlerID]);
   }
@@ -29,35 +33,72 @@ namespace mtca4u{
     return spiRead( SMDA_COMMON, JDX_CHIP_VERSION ).getDATA();
   }
 
-  TMC429Register MotorDriverCardImpl::spiRead( unsigned int smda, unsigned int idx_jdx )
+  TMC429Word MotorDriverCardImpl::spiRead( unsigned int smda, unsigned int idx_jdx )
   {
     // Although the first half is almost identical to the spiWrite,
-    // the preparation of the TMC429Register is different. So we accept a little 
+    // the preparation of the TMC429Word is different. So we accept a little 
     // code duplication here.
-    TMC429Register * readRequest = new TMC429Register;
-    std::auto_ptr<TMC429Register> readRequest(new TMC429Register);
+    TMC429Word readRequest;
     readRequest.setSMDA(smda);
     readRequest.setIDX_JDX(idx_jdx);
-    readRequest.setRW(READ);
+    readRequest.setRW(RW_READ);
 
-    _psiControlWriteMarshaller.writeRaw(readRequest.dataWord());
+    // we need this intermediate variable because the write function needs a pointer
+    // to int.
+    int writeMe = static_cast<int>(readRequest.getDataWord());
+    _spiControlWriteRegister.writeReg(&writeMe, sizeof(int));
     //FIXME: synchronise so readback value is valid
-    unsigned int readbackValue;
-    _psiControlReadbackMarshaller.readRaw( & readbackValue );
+    
+    int readbackValue;
+    _spiControlReadbackRegister.readReg( & readbackValue, sizeof(int));
 
-    return TMC429Register(readbackValue);
+    return TMC429Word(static_cast<unsigned int>(readbackValue));
   }
 
   void MotorDriverCardImpl::spiWrite( unsigned int smda, unsigned int idx_jdx, 
-				  unsigned int data );
+				      unsigned int data )
   {
-    TMC429Register writeMe;
-    readRequest.setSMDA(smda);
-    readRequest.setIDX_JDX(idx_jdx);
-    readRequest.setRW(WRITE);
-    readRequest.setDATA(data);
+    TMC429Word writeMe;
+    writeMe.setSMDA(smda);
+    writeMe.setIDX_JDX(idx_jdx);
+    writeMe.setRW(RW_WRITE);
+    writeMe.setDATA(data);
 
-    _psiControlWriteMarshaller.writeRaw( writeMe.dataWord());
+    int temporaryWriteInt = static_cast<int>(writeMe.getDataWord());
+    _spiControlWriteRegister.writeReg( &temporaryWriteInt, sizeof(int));
+  }
+
+  void MotorDriverCardImpl::setDatagramLowWord(unsigned int datagramLowWord){
+  }
+
+  void MotorDriverCardImpl::setDatagramHighWord(unsigned int datagramHighWord){
+  }
+   
+  void MotorDriverCardImpl::setCoverPositionAndLength(
+		CoverPositionAndLength const & coverPositionAndLength){
+  }
+
+  void MotorDriverCardImpl::setCoverDatagram(unsigned int coverDatagram){
+  }
+
+  
+  void MotorDriverCardImpl::setStepperMotorGlobalParametersRegister(
+	   StepperMotorGlobalParameters const & stepperMotorGlobalParameters){
+  }
+
+  void MotorDriverCardImpl::setInterfaceConfiguration(
+	   InterfaceConfiguration const & interfaceConfiguration){
+  }
+
+  void MotorDriverCardImpl::setPositionCompareRegister(
+           unsigned int positionCompareWord){
+  }
+
+  void MotorDriverCardImpl::setPositionCompareInterruptRegister(
+	   PositionCompareInterruptData const & positionCompareInterruptData){
+  }
+
+  void MotorDriverCardImpl::powerDown(){
   }
 
 }// namespace mtca4u
