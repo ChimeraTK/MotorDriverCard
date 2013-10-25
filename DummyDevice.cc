c46ef8955846f80622e64382b40f1de7eeb09a94
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2013-10-25 12:09:38 +0000

    started implementing a generic dummy device. Tests on FakeRegister complete and working.
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/trunk/src@132 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ DummyDevice.cc
@@ -0,0 +1,133 @@
+#include "DummyDevice.h"
+#include "NotImplementedException.h"
+#include <MtcaMappedDevice/mapFileParser.h>
+
+// Valid bar numbers are 0 to 5 , so they must be contained
+// in three bits.
+#define BAR_MASK 0x7
+// the bar number is stored in bits 60 to 62
+#define BAR_POSITION_IN_VIRTUAL_REGISTER 60
+
+namespace mtca4u{
+
+  DummyDevice::FakeRegister::FakeRegister(size_t sizeInWords_) : _dataContent(sizeInWords_, 0),
+								_isReadOnly(false)
+  {}
+
+  size_t DummyDevice::FakeRegister::sizeInWords(){
+    return _dataContent.size();
+  }
+
+  void DummyDevice::FakeRegister::setReadOnly(bool isReadOnly_){
+    _isReadOnly = isReadOnly_;
+  }
+
+  bool  DummyDevice::FakeRegister::isReadOnly(){
+    return _isReadOnly;
+  }
+
+  int32_t DummyDevice::FakeRegister::read(size_t offset){
+    // we intentionally use at() and not [] to have a range check
+    return _dataContent.at(offset);
+  }
+
+  void DummyDevice::FakeRegister::write(int32_t dataWord, size_t offset){
+    if (_isReadOnly){
+      return;
+    }
+    
+    // we intentionally use at() and not [] to have a range check
+    _dataContent.at(offset) = dataWord;
+
+    // call the writeCallbackFunction, if it has been set
+    if (_writeCallbackFunction){
+      _writeCallbackFunction();
+    }
+  }
+
+  void DummyDevice::FakeRegister::setWriteCallbackFunction(
+	    boost::function<void(void)> writeCallbackFunction ){
+    _writeCallbackFunction =  writeCallbackFunction;
+  }
+
+  //Noting to do, intentionally empty.
+  DummyDevice::DummyDevice(){
+  }
+
+  //Nothing to clean up, all objects clean up for themselves when
+  //they go out of scope.
+  DummyDevice::~DummyDevice(){
+  }
+
+  void DummyDevice::openDev(const std::string &mappingFileName,
+			    int /* perm */, devConfigBase* /* pConfig */){
+    _registerMapping = mapFileParser().parse(mappingFileName);
+    populateRegisterMap();
+    opened=false;
+  }
+
+  void DummyDevice::populateRegisterMap(){
+    for (mapFile::const_iterator mappingElementIter = _registerMapping->begin();
+	 mappingElementIter <  _registerMapping->end(); ++mappingElementIter ) {
+      addOrResizeRegister( *mappingElementIter );
+    }     
+  }
+
+  void DummyDevice::addOrResizeRegister( mapFile::mapElem const & mappingElement ){
+    std::map< uint64_t, FakeRegister >::iterator targetEnty = 
+      _registers.find( calculateVirtualRegisterAddress( mappingElement.reg_address,
+							mappingElement.reg_bar ) );
+
+    if (targetEnty == _registers.end()){
+      
+    }
+    else{
+    }
+      
+   }
+
+  void DummyDevice::closeDev(){
+    _registerMapping = ptrmapFile(0);
+    _registers.clear();
+    opened=false;
+  }
+
+  void DummyDevice::readReg(uint32_t regOffset, int32_t* data, uint8_t bar){
+    throw NotImplementedException("DummyDevice::readReg is not implemented yet.");
+  }
+
+  void DummyDevice::writeReg(uint32_t regOffset, int32_t data, uint8_t bar){
+    throw NotImplementedException("DummyDevice::writeReg is not implemented yet.");
+  }
+
+  void DummyDevice::readArea(uint32_t regOffset, int32_t* data, size_t size,
+			     uint8_t bar){
+    throw NotImplementedException("DummyDevice::readArea is not implemented yet.");
+  }
+
+  void DummyDevice::writeArea(uint32_t regOffset, int32_t* data, size_t size,
+			      uint8_t bar){
+    throw NotImplementedException("DummyDevice::writeArea is not implemented yet.");
+  }
+
+  void DummyDevice::readDMA(uint32_t regOffset, int32_t* data, size_t size,
+			    uint8_t bar){
+    throw NotImplementedException("DummyDevice::readDMA is not implemented yet.");
+  }
+
+  void DummyDevice::writeDMA(uint32_t regOffset, int32_t* data, size_t size,
+			     uint8_t bar){
+    throw NotImplementedException("DummyDevice::writeDMA is not implemented yet.");
+  }
+
+  void DummyDevice::readDeviceInfo(std::string* devInfo){
+    throw NotImplementedException("DummyDevice::readDeviceInfo is not implemented yet.");
+  }
+
+  uint64_t DummyDevice::calculateVirtualRegisterAddress( uint32_t registerOffsetInBar,
+							 uint8_t bar){
+    return (static_cast<uint64_t>(bar & BAR_MASK) << BAR_POSITION_IN_VIRTUAL_REGISTER) | 
+           (static_cast<uint64_t> (registerOffsetInBar));
+  }
+  
+}// namespace mtca4u
