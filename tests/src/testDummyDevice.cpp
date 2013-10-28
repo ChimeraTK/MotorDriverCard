f8ffcdf151f4a1e323bce7b32481b96bbded6a60
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2013-10-28 18:57:09 +0000

    all tests for implemented functions of DummyDevice. 100% code coverage, but functions and branches are low
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/trunk@138 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/src/testDummyDevice.cpp
+++ tests/src/testDummyDevice.cpp
@@ -1,14 +1,19 @@
 #include <boost/test/included/unit_test.hpp>
 using namespace boost::unit_test_framework;
+#include <boost/lambda/lambda.hpp>
+#include <boost/function.hpp>
+#include <boost/bind.hpp>
+
+#include "DummyDevice.h"
+#include "NotImplementedException.h"
+using namespace mtca4u;
 
 #define TEST_MAPPING_FILE "utcadummy.map"
 #define FIRMWARE_REGISTER_STRING "WORD_FIRMWARE"
 #define STATUS_REGISTER_STRING "WORD_STATUS"
 #define USER_REGISTER_STRING "WORD_USER"
 #define CLOCK_MUX_REGISTER_STRING "WORD_CLK_MUX"
-
-#include "DummyDevice.h"
-using namespace mtca4u;
+#define CLOCK_RESET_REGISTER_STRING "WORD_CLK_RST"
 
 // forward declaration so we can declare it friend
 // of TestableDummyDevice.
@@ -31,9 +36,16 @@ public:
   static void testCheckSizeIsMultipleOfWordSize();
   void testOpenClose();
   void testReadWriteSingleWordRegister();
+  /// The read function can be readDMA or readArea, the writeFunction is always writeArea.
+  /// WriteDMA probably does not make sense at all. 
+  void testReadWriteMultiWordRegister(void(DummyDevice::* readFunction)(uint32_t, int32_t*, size_t, uint8_t));
+  void testWriteDMA();
+  void testReadDeviceInfo();
 
 private:
   TestableDummyDevice _dummyDevice;
+  void freshlyOpenDevice();
+  friend class DummyDeviceTestSuite;
 };
 
 class  DummyDeviceTestSuite : public test_suite{
@@ -43,10 +55,22 @@ class  DummyDeviceTestSuite : public test_suite{
     
     test_case* openCloseTestCase = BOOST_CLASS_TEST_CASE( &DummyDeviceTest::testOpenClose, dummyDeviceTest );
 
+    boost::function<void(void)> testReadWriteArea 
+      = boost::bind( &DummyDeviceTest::testReadWriteMultiWordRegister,
+		     dummyDeviceTest, &DummyDevice::readArea);
+
+    boost::function<void(void)> testReadWriteDMA 
+      = boost::bind( &DummyDeviceTest::testReadWriteMultiWordRegister,
+		     dummyDeviceTest, &DummyDevice::readDMA);
+
     add( BOOST_TEST_CASE( DummyDeviceTest::testCalculateVirtualRegisterAddress ) );
     add( BOOST_TEST_CASE( DummyDeviceTest::testCheckSizeIsMultipleOfWordSize ) );
     add( openCloseTestCase );
     add( BOOST_CLASS_TEST_CASE( &DummyDeviceTest::testReadWriteSingleWordRegister, dummyDeviceTest ) );
+    add( BOOST_TEST_CASE( testReadWriteArea ) );
+    add( BOOST_TEST_CASE( testReadWriteDMA ) );
+    add( BOOST_CLASS_TEST_CASE( &DummyDeviceTest::testWriteDMA, dummyDeviceTest ) );
+    add( BOOST_CLASS_TEST_CASE( &DummyDeviceTest::testReadDeviceInfo, dummyDeviceTest ) );
   }
 };
 
@@ -110,18 +134,10 @@ void DummyDeviceTest::testOpenClose(){
 }
 
 void DummyDeviceTest::testReadWriteSingleWordRegister(){
-  try{
-    _dummyDevice.openDev(TEST_MAPPING_FILE);
-  }
-  catch(DummyDeviceException &){
-    // make sure the device was freshly opened, so
-    // registers are set to 0.
-    _dummyDevice.closeDev();
-    _dummyDevice.openDev(TEST_MAPPING_FILE);
-  }
+  freshlyOpenDevice();
 
   mapFile::mapElem mappingElement;
-  _dummyDevice._registerMapping->getRegisterInfo (FIRMWARE_REGISTER_STRING, mappingElement);
+  _dummyDevice._registerMapping->getRegisterInfo (CLOCK_RESET_REGISTER_STRING, mappingElement);
 
   uint32_t offset =  mappingElement.reg_address;
   uint8_t bar =  mappingElement.reg_bar;
@@ -142,10 +158,108 @@ void DummyDeviceTest::testReadWriteSingleWordRegister(){
   BOOST_CHECK( dataContent == 47 );
 
   // the size as index is invalid, allowed range is 0..size-1 included.
-  BOOST_CHECK_THROW( _dummyDevice.readReg(  _dummyDevice._barContents[bar].size(),
+  BOOST_CHECK_THROW( _dummyDevice.readReg(  _dummyDevice._barContents[bar].size()*sizeof(int32_t),
 					    &dataContent, bar),
 		     DummyDeviceException );
-  BOOST_CHECK_THROW( _dummyDevice.writeReg(  _dummyDevice._barContents[bar].size(),
+  BOOST_CHECK_THROW( _dummyDevice.writeReg(  _dummyDevice._barContents[bar].size()*sizeof(int32_t),
 					     dataContent, bar),
 		     DummyDeviceException );
 }
+
+//void DummyDeviceTest::testReadWriteMultiWordRegister(boost::function<void(uint32_t, int32_t*, size_t, uint8_t)> readFunction){
+void DummyDeviceTest::testReadWriteMultiWordRegister(void(DummyDevice::* readFunction)(uint32_t, int32_t*, size_t, uint8_t)){
+  freshlyOpenDevice();
+
+  mapFile::mapElem mappingElement;
+  _dummyDevice._registerMapping->getRegisterInfo (CLOCK_MUX_REGISTER_STRING, mappingElement);
+
+  uint32_t offset =  mappingElement.reg_address;
+  uint8_t bar =  mappingElement.reg_bar;
+  size_t sizeInBytes =  mappingElement.reg_size;
+  size_t sizeInWords =  mappingElement.reg_size / sizeof(int32_t);
+  std::vector<int32_t> dataContent(sizeInWords, -1);
+
+  BOOST_CHECK_NO_THROW( (_dummyDevice.*readFunction)( offset, 
+						      &(dataContent[0]),
+						      sizeInBytes,
+						      bar ) );
+  for( std::vector<int32_t>::iterator dataIter = dataContent.begin();
+       dataIter != dataContent.end(); ++dataIter){
+    std::stringstream errorMessage;
+    errorMessage << "*dataIter = " <<  *dataIter;
+    BOOST_CHECK_MESSAGE( *dataIter == 0, errorMessage.str() );
+  }
+
+  for( unsigned int index = 0; index < dataContent.size(); ++index ){
+    dataContent[index] = static_cast<int32_t>((index+1) * (index+1));
+  }
+  BOOST_CHECK_NO_THROW( _dummyDevice.writeArea( offset, 
+						&(dataContent[0]),
+						sizeInBytes,
+						bar ) );  
+  // make sure the value is really being read
+  std::for_each( dataContent.begin(), dataContent.end(), boost::lambda::_1 = -1);
+
+  // no need to test NO_THROW on the same register twice
+  _dummyDevice.readArea( offset, &(dataContent[0]), sizeInBytes, bar );
+
+   // make sure the value is really being read
+  for( unsigned int index = 0; index < dataContent.size(); ++index ){
+    BOOST_CHECK( dataContent[index] == static_cast<int32_t>((index+1) * (index+1)) );
+  }
+
+  // tests for exceptions
+  // 1. base address too large
+  BOOST_CHECK_THROW( _dummyDevice.readArea( _dummyDevice._barContents[bar].size()*sizeof(int32_t),
+					    &(dataContent[0]), sizeInBytes, bar),
+		     DummyDeviceException );
+  BOOST_CHECK_THROW( _dummyDevice.writeArea( _dummyDevice._barContents[bar].size()*sizeof(int32_t),
+					     &(dataContent[0]), sizeInBytes, bar),
+		     DummyDeviceException );
+  // 2. size too large (works because the target register is not at offfset 0)
+  // resize the data vector for this test
+  dataContent.resize(_dummyDevice._barContents[bar].size());
+  BOOST_CHECK_THROW( _dummyDevice.readArea( offset, &(dataContent[0]),
+					    _dummyDevice._barContents[bar].size()*sizeof(int32_t),
+					    bar),
+		     DummyDeviceException );
+  BOOST_CHECK_THROW( _dummyDevice.writeArea( offset, &(dataContent[0]),
+					     _dummyDevice._barContents[bar].size()*sizeof(int32_t),
+					     bar),
+		     DummyDeviceException );
+  // 3. size not multiple of 4
+  BOOST_CHECK_THROW( _dummyDevice.readArea( offset, &(dataContent[0]),
+					    sizeInBytes-1,
+					    bar),
+		     DummyDeviceException );
+  BOOST_CHECK_THROW( _dummyDevice.writeArea( offset, &(dataContent[0]),
+					     sizeInBytes-1,
+					     bar),
+		     DummyDeviceException );
+}
+
+void DummyDeviceTest::freshlyOpenDevice(){
+  try{
+    _dummyDevice.openDev(TEST_MAPPING_FILE);
+  }
+  catch(DummyDeviceException &){
+    // make sure the device was freshly opened, so
+    // registers are set to 0.
+    _dummyDevice.closeDev();
+    _dummyDevice.openDev(TEST_MAPPING_FILE);
+  }
+}
+
+void DummyDeviceTest::testWriteDMA(){
+  // will probably never be implemented
+  BOOST_CHECK_THROW( _dummyDevice.writeDMA( 0, NULL, 0, 0 ), 
+		     NotImplementedException );
+}
+
+void DummyDeviceTest::testReadDeviceInfo(){
+  std::string deviceInfo;
+  _dummyDevice.readDeviceInfo(&deviceInfo);
+  BOOST_CHECK( deviceInfo == (std::string("DummyDevice with mapping file ") 
+			      + TEST_MAPPING_FILE) );
+}
+
