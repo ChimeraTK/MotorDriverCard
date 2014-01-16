fa3345e3cde00918a4891ca90a56c86704c45073
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-01-16 23:05:12 +0000

    added scripts for debian packages
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/trunk@263 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ cmake/prepare_debian_package.cmake
@@ -0,0 +1,51 @@
+#Prepare the debian control files from the template.
+#Basically this is setting the correct version number in most of the files
+
+#The debian version string must not contain ".", so we use "-"
+set(MotorDriverCard_DEBVERSION ${MotorDriverCard_MAJOR_VERSION}-${MotorDriverCard_MINOR_VERSION})
+
+#Nothing to change, just copy
+file(COPY ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/compat
+           ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/rules
+     DESTINATION debian_from_template)
+
+file(COPY ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/source/format
+     DESTINATION debian_from_template/source)
+
+#Adapt the file name
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/mtca4u-motordrivercardDEBVERSION.install
+               debian_from_template/mtca4u-motordrivercard${MotorDriverCard_DEBVERSION}.install COPYONLY)
+
+#Adapt the file name and/or set the version number
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/control.in
+               debian_from_template/control @ONLY)
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/copyright.in
+               debian_from_template/copyright @ONLY)
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/mtca4u-motordrivercardDEBVERSION.shlib.in
+               debian_from_template/mtca4u-motordrivercard${MotorDriverCard_DEBVERSION}.shlib @ONLY)
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/dev-mtca4u-motordrivercard.install.in
+               debian_from_template/dev-mtca4u-motordrivercard.install @ONLY)
+
+#Copy and configure the shell script which performs the actual 
+#building of the package
+configure_file(${CMAKE_SOURCE_DIR}/cmake/make_debian_package.sh.in
+               make_debian_package.sh @ONLY)
+
+#A custom target so you can just run make debian_package
+#(You could instead run make_debian_package.sh yourself, hm...)
+add_custom_target(debian_package ${CMAKE_BINARY_DIR}/make_debian_package.sh
+                  COMMENT Building debian package for tag ${MotorDriverCard_VERSION})
+
+#For convenience: Also create an install script for DESY
+#The shared library package has the version number in the package name
+set(PACKAGE_NAME "mtca4u-motordrivercard${MotorDriverCard_DEBVERSION}")
+#The development package does not have the version in the name
+set(PACKAGE_DEV_NAME "dev-mtca4u-motordrivercard")
+set(PACKAGE_FILES_WILDCARDS "${PACKAGE_NAME}_*.deb ${PACKAGE_DEV_NAME}_*.deb mtca4u-motordrivercard_*.changes")
+
+configure_file(${CMAKE_SOURCE_DIR}/cmake/install_debian_package_at_DESY.sh.in
+               install_debian_package_at_DESY.sh @ONLY)
+
