374a6c7d31dcdf487564333e21e38124d189612e
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-01-29 13:45:28 +0000

    significant improvement of forwardBackward programme - config is read from xml file - end switch detection - endless loop - clean exit with catching ^C and turning off the motor
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/trunk@293 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/hardware/src/forwardBackardDFMC-MD22.cpp
+++ tests/hardware/src/forwardBackardDFMC-MD22.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 
 #include "MotorDriverCardImpl.h"
+#include "MotorDriverCardConfigXML.h"
 #include <unistd.h>
+#include <signal.h>
+
+bool volatile keepRunning = true;
+
+void intHandler(int /*dummy*/ = 0) {
+    keepRunning = false;
+}
+
+#define CHECK_EXIT_ON_SIGNAL\
+  if(!keepRunning){\
+    motor0.setEnabled(false);\
+    std::cout << std::endl << "Caught signal, ending programme." << std::endl; \
+    return 0;\
+  }
 
 using namespace mtca4u;
 
 int main( int argc, char* argv[] )
 {
-  if (argc !=2){
-    std::cout << "Move to motor forward, wait a second and move back to the original position."
+  if (argc !=4){
+    std::cout << "Move to the 'left' (low values) end switch. Afterward move infinetly between 0.2*nStepsMax and nStepsMax."
 	      << std::endl << std::endl;
-    std::cout << "Usage: " << argv[0] << " dmapFile" << std::endl;
+    std::cout << "Usage: " << argv[0] << " dmapFile MotorDriverConfig.xml nStepsMax" << std::endl;
     return -1;
   }
+
+  signal(SIGINT, intHandler);
 
   std::pair<std::string, std::string> deviceFileAndMapFileName =
     dmapFileParser().parse( argv[1] )->begin()->getDeviceFileAndMapFileName();
@@ -25,73 +42,103 @@ int main( int argc, char* argv[] )
   boost::shared_ptr< devMap< devBase > > mappedDevice( new devMap<devBase> );
   mappedDevice->openDev( ioDevice, registerMapping );
 
-  mtca4u::MotorDriverCardImpl motorDriverCard( mappedDevice , mtca4u::MotorDriverCardConfig());
+  mtca4u::MotorDriverCardConfig motorDriverCardConfig = MotorDriverCardConfigXML::read( argv[2] );
+
+  mtca4u::MotorDriverCardImpl motorDriverCard( mappedDevice , motorDriverCardConfig );
 
   // until now it is a copy of initialiseDFMC-MC22, now we start moving the motor
 
+  unsigned int nStepsMax = (strtoul( argv[3] , NULL, 0 ) & 0x7FFFFF);// limit to the maximal possible position
+
   mtca4u::MotorControler & motor0 = motorDriverCard.getMotorControler(0);
-
-  // Adapt the settings to the VT-21 motor.
-  // FIXME: read in the config from a file
-
-  // filtered stallGuard with 3/32 current scale
-  motor0.setStallGuardControlData( mtca4u::StallGuardControlData(0x10002) );
-  // coolStep with 1/4 CS minimum current (taken directly from the data sheet, no clue if it works)
-  motor0.setCoolStepControlData( mtca4u::CoolStepControlData(0x8202) );
-  // 16 microsteps per 90 degrees
-  motor0.setDriverControlData( mtca4u::DriverControlData(0x4) );
 
   try{
     motor0.setEnabled(true);
+    
+    std::cout << "moving to left end switch" << std::endl;
 
-    std::cout << std::hex << "switches are 0x" << motorDriverCard.getReferenceSwitchData().getDataWord() 
-	      << std::dec << std::endl;
+    // move as far left as possible
+    motor0.setActualPosition(0x7FFFFF); // the maximal possible position
+    motor0.setTargetPosition(0x100000); // the minimal possible position
 
-    mtca4u::ReferenceConfigAndRampModeData referenceConfigAndRampModeData;
-    //referenceConfigAndRampModeData.setDISABLE_STOP_L(true);
-    //referenceConfigAndRampModeData.setDISABLE_STOP_R(true);
-    motor0.setReferenceConfigAndRampModeData( referenceConfigAndRampModeData );
-
-    std::cout << "Position is " << motor0.getActualPosition() << std::endl;
-    std::cout << "Starting the motor..." << std::endl;
-
-    motor0.setTargetPosition(50000);
-
-    try{
-      while (motor0.getActualPosition() != motor0.getTargetPosition() ){
-	std::cout << "\r Position: " << motor0.getActualPosition()  << "    "
-		  << " ,    switches are : 0x" << std::hex 
-		  << motorDriverCard.getReferenceSwitchData().getDataWord() << std::dec
-		  << "    " << std::flush;
+    while(  motorDriverCard.getReferenceSwitchData().getLeft1() != 0x1 ){
+      std::cout << "\r Position: " << motor0.getActualPosition()<< std::hex << " ,  switches are 0x" 
+		<< motorDriverCard.getReferenceSwitchData().getDATA()
+		<< "        " << std::dec <<std::flush;
+      if (motorDriverCard.getReferenceSwitchData().getRight1() ){
+	std::cout << std::endl << "WARNING: Right end switch went active when the left switch was expected!"
+		  << " Check you end switch cabeling!" 
+		  << std::endl;
+	break;
       }
-    }catch(mtca4u::MotorDriverException &e){
-      std::cout << "Problem reading target position" << std::endl;
+      
+      CHECK_EXIT_ON_SIGNAL
+
+      // FIXME: Check on motor stall / motor is still moving.
     }
+    
     std::cout << std::endl;
 
-    sleep(1);
+    //    std::cout << std::hex << "switches are 0x" << motorDriverCard.getReferenceSwitchData().getDataWord() 
+    //	      << std::dec << std::endl;
 
-    std::cout << "Maximum position reached: " << motor0.getActualPosition() << std::endl;
-    std::cout << std::endl;
-    std::cout << "Moving back..." << std::endl;
+    //    std::cout << "Position is " << motor0.getActualPosition() << std::endl;
+    std::cout << "Setting left switch to position 0 " <<std::endl;
 
+    motor0.setEnabled(false);
+    motor0.setActualPosition(0);
     motor0.setTargetPosition(0);
-     try{
-       while (motor0.getActualPosition() != motor0.getTargetPosition() ){
-	 std::cout << "\r Position: " << motor0.getActualPosition() 
-		   << " ,    switches are : 0x" << std::hex 
-		   << motorDriverCard.getReferenceSwitchData().getDataWord() << std::dec
-		   << "    " << std::flush;
-       }
-     }catch(mtca4u::MotorDriverException &e){
-       std::cout << "Problem reading target position" << std::endl;
-     }
-     std::cout << std::endl;
-  
-     std::cout << "Original position reached: " << motor0.getActualPosition() << std::endl;
+    motor0.setEnabled(true);
 
-     std::cout << "Turning off the motor." << std::endl;
-     motor0.setEnabled(false);
+    std::cout <<"Starting the forward-backward loop ..." << std::endl;
+
+    while (1){
+      motor0.setTargetPosition( nStepsMax );
+      std::cout << std::endl<< "Moving forward..." << std::endl;
+
+      try{
+	while (motor0.getActualPosition() != motor0.getTargetPosition() ){
+	  std::cout << "\r Position: " << motor0.getActualPosition()  << "    "
+		    << " ,    switches are : 0x" << std::hex 
+		    << motorDriverCard.getReferenceSwitchData().getDataWord() << std::dec
+		    << "    " << std::flush;
+	  if( motorDriverCard.getReferenceSwitchData().getRight1() ){
+	    nStepsMax = static_cast<unsigned int>( 0.8*motor0.getActualPosition() );
+	  std::cout << std::endl <<"Right end switch reached. Adapting nStepsMax to " << nStepsMax << std::endl;
+	    break;
+	  }
+	  CHECK_EXIT_ON_SIGNAL	  
+	}
+      }catch(mtca4u::MotorDriverException &e){
+	std::cout << "Problem reading target position" << std::endl;
+      }
+      std::cout << std::endl;
+
+      std::cout << "Maximum position reached: " << motor0.getActualPosition() << std::endl;
+      std::cout << std::endl;
+      std::cout << "Moving back..." << std::endl;
+
+      motor0.setTargetPosition( static_cast<unsigned int>( 0.2 * nStepsMax) );
+      try{
+	while (motor0.getActualPosition() != motor0.getTargetPosition() ){
+	  std::cout << "\r Position: " << motor0.getActualPosition() 
+		    << " ,    switches are : 0x" << std::hex 
+		    << motorDriverCard.getReferenceSwitchData().getDataWord() << std::dec
+		    << "    " << std::flush;
+	  if( motorDriverCard.getReferenceSwitchData().getLeft1() ){
+	    std::cout << "Left end switch reached. Adapting calibration." << std::endl;
+	    motor0.setActualPosition(0);
+	    break;
+	  }
+	  CHECK_EXIT_ON_SIGNAL
+	}
+      }catch(mtca4u::MotorDriverException &e){
+	std::cout << "Problem reading target position" << std::endl;
+      }
+      std::cout << std::endl;
+  
+      std::cout << "Minimal position reached: " << motor0.getActualPosition() << std::endl;
+  }
   }
   catch(...){
     motor0.setEnabled(false);
