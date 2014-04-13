d64efd681f3041316844225c71d54e964598f1de
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-04-13 22:09:50 +0000

    implemented converter for arbitrary length signed int convertion. Not used in the other classes yet
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/trunk/src@334 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ SignedIntConverter.cc
@@ -0,0 +1,26 @@
+#include "SignedIntConverter.h"
+
+namespace mtca4u{
+  
+  /// Fixme: is this safe in case someone puts nBits > 31?
+  SignedIntConverter::SignedIntConverter( unsigned int nBits )
+    : _nBits(nBits) , _bitMask( (1<<nBits) -1 ), _negativeBit( 1 << (nBits-1) )
+  {}
+
+  int32_t SignedIntConverter::customToThirtyTwo(int32_t const & customInt){
+    if( customInt & _negativeBit){
+      // fill in the leading bits up to 32 bits
+      return customInt | ~_bitMask;
+    }
+    else{
+      return customInt;
+    }
+  }
+  
+  int32_t SignedIntConverter::thrityTwoToCustom(int32_t const & thirtyTwoBitInt){
+    // just crop the 32 bit int. no range check
+    return thirtyTwoBitInt & _bitMask;
+  }
+  
+
+}//namespace mtca4u
