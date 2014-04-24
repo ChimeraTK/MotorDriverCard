e249750782f1259e6ce461e6dcf011153f67a95e
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-04-24 11:36:43 +0000

    put reading/writing to TMC429 into separate class, so it can be used in MotorDriverCard and MotorControler
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/branches/SPI_Handshake@352 5f601a5c-88b9-47f3-b851-818f6741d00d

---
+++ src/TMC429SPI.cc
@@ -0,0 +1,42 @@
+#include "TMC429SPI.h"
+#include "TMC429Constants.h"
+
+namespace mtca4u{
+
+  TMC429SPI::TMC429SPI(  boost::shared_ptr< devMap<devBase> > const & mappedDevice,
+			 std::string const & writeRegisterName,
+			 std::string const & syncRegisterName,
+			 std::string const & readbackRegisterName,
+			 unsigned int spiWaitingTime)
+    : _spiViaPCIe(mappedDevice, writeRegisterName, syncRegisterName, readbackRegisterName, spiWaitingTime)
+  {}
+
+  TMC429OutputWord TMC429SPI::read( unsigned int smda, unsigned int idx_jdx ){
+    // Although the first half is almost identical to write,
+    // the preparation of the TMC429InputWord is different. So we accept a little 
+    // code duplication here.
+    TMC429InputWord readRequest;
+    readRequest.setSMDA(smda);
+    readRequest.setIDX_JDX(idx_jdx);
+    readRequest.setRW(tmc429::RW_READ);
+
+    return TMC429OutputWord( _spiViaPCIe.read( readRequest.getDataWord() ) );
+  }
+
+  void TMC429SPI::write( unsigned int smda, unsigned int idx_jdx, unsigned int data )
+  {
+    TMC429InputWord writeMe;
+    writeMe.setSMDA(smda);
+    writeMe.setIDX_JDX(idx_jdx);
+    writeMe.setRW(tmc429::RW_WRITE);
+    writeMe.setDATA(data);
+    
+    write( writeMe );
+  }
+
+  void TMC429SPI::write( TMC429InputWord const & writeWord ){
+    _spiViaPCIe.write( writeWord.getDataWord() );
+  }
+
+
+}//namespace mtca4u
---
+++ tests/src/testTMC429SPI.cc
@@ -0,0 +1,95 @@
+#include <sstream>
+
+#include <boost/test/included/unit_test.hpp>
+using namespace boost::unit_test_framework;
+
+#include "DFMC_MD22Constants.h"
+#include <MtcaMappedDevice/devMap.h>
+//#include <MtcaMappedDevice/libmap.h>
+#include "TMC429SPI.h"
+#include "DFMC_MD22Dummy.h"
+#include "DFMC_MD22Constants.h"
+#include "TMC429Words.h"
+#include "TMC429Constants.h"
+#include "MotorDriverException.h" 
+using namespace mtca4u;
+using namespace mtca4u::dfmc_md22;
+using namespace mtca4u::tmc429;
+#include "testWordFromSpiAddress.h"
+
+#define MAP_FILE_NAME "DFMC_MD22_test.map"
+
+class TMC429SPITest{
+ public:
+  
+  TMC429SPITest(std::string const & mapFileName);
+  //  static void testConstructors();
+
+  void testRead();
+  void testWrite();
+
+ private:
+  boost::shared_ptr<DFMC_MD22Dummy> _dummyDevice;
+  boost::shared_ptr< devMap<devBase> > _mappedDevice;
+  std::string _mapFileName;
+
+  boost::shared_ptr<TMC429SPI> _tmc429Spi;
+};
+
+class TMC429SPITestSuite : public test_suite {
+public:
+  TMC429SPITestSuite(std::string const & mapFileName): test_suite("TMC429SPI test suite") {
+    // create an instance of the test class
+    boost::shared_ptr<TMC429SPITest> tmc429SpiTest( new TMC429SPITest(mapFileName) );
+
+    // in case of dependencies store the test cases before adding them and declare the dependency
+    test_case* readTestCase = BOOST_CLASS_TEST_CASE( &TMC429SPITest::testRead, tmc429SpiTest );
+    test_case* writeTestCase = BOOST_CLASS_TEST_CASE( &TMC429SPITest::testWrite, tmc429SpiTest );
+
+    writeTestCase->depends_on(readTestCase);
+
+    add( readTestCase );
+    add( writeTestCase );
+  }
+};
+
+test_suite*
+init_unit_test_suite( int /*argc*/, char* /*argv*/ [] )
+{
+  framework::master_test_suite().p_name.value = "TMC429SPI test suite";
+  return new TMC429SPITestSuite( MAP_FILE_NAME );
+}
+
+TMC429SPITest::TMC429SPITest(std::string const & mapFileName)
+  :  _dummyDevice( new DFMC_MD22Dummy ), _mappedDevice(new devMap<devBase>){
+  /// we need a mapped device of devBase. Unfortunately this is still really clumsy to produce/open
+  _dummyDevice->openDev( mapFileName );
+ 
+  mapFileParser fileParser;
+  boost::shared_ptr<mapFile> registerMapping = fileParser.parse(mapFileName);
+
+  _mappedDevice->openDev( _dummyDevice, registerMapping );
+  
+  _dummyDevice->setRegistersForTesting();
+
+   _tmc429Spi.reset( new TMC429SPI( _mappedDevice, CONTROLER_SPI_WRITE_ADDRESS_STRING,
+				    CONTROLER_SPI_SYNC_ADDRESS_STRING,
+				    CONTROLER_SPI_READBACK_ADDRESS_STRING ) );
+}
+
+void TMC429SPITest::testRead(){
+  // use the cover datagram for testing
+  unsigned int expectedContent = testWordFromSpiAddress( SMDA_COMMON,
+							 JDX_COVER_DATAGRAM);
+  TMC429OutputWord readResponse =  _tmc429Spi->read(  SMDA_COMMON, JDX_COVER_DATAGRAM);
+  BOOST_CHECK( readResponse.getDATA() == expectedContent );
+}
+
+void TMC429SPITest::testWrite(){
+  unsigned int expectedContent = testWordFromSpiAddress( SMDA_COMMON,
+							 JDX_COVER_DATAGRAM);
+  ++expectedContent;
+  _tmc429Spi->write( SMDA_COMMON, JDX_COVER_DATAGRAM, expectedContent);
+
+  BOOST_CHECK( _tmc429Spi->read(SMDA_COMMON, JDX_COVER_DATAGRAM).getDATA() == expectedContent );
+}
