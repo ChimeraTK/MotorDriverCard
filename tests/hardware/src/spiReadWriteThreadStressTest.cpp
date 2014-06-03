c7fcdeffe8a86716ea0fb6f025811ed4fe507e56
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-06-03 12:18:50 +0000

    improved spiReadWriteThreadStressTest, still only one test but reasonable error reporting
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/branches/StepperMotor@404 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/hardware/src/spiReadWriteThreadStressTest.cpp
+++ tests/hardware/src/spiReadWriteThreadStressTest.cpp
@@ -9,6 +9,43 @@
 
 using namespace mtca4u;
 
+/** The SpiReadWriteThreadStressTest performs cycles of spi communication tests, each
+ *  running a sequence of different combinations of read/write access to the
+ *  TMC429 controller chip or the TMC260 driver chip.
+ *
+ *  The building blocks are the following loops/threads:
+ *  -# Write/readback/check the target position regsiter of TMC429 (motor 0 or 1)
+ *  -# Write to the TMC429 PositionCompareInterrupt register of motor 0 as fast
+ *     as possible to cause possible coflicts in other threads.
+ *     Does not check if writing is successful.
+ *  -# Read the actual position of motor 1 as fast as possible to TMC429.
+ *  -# Read theTMC429 reference switch register from the global section 
+ *     as fast as possible.
+ *  -# Alternatingly write 0x1F and 0x7 to the current scaling sub-word of the
+ *     StallGuardControl register, and check for consistency in the answer with
+ *     RDSEL=0x2 (read selection word of the CoolStepControl register), for motor 0/1
+ *     The loop can be configured to have a delay between each change.
+ *  -# Alternatingly write 0xF and 0x0 to the MRES (micro step resolution) word
+ *     in the DriverControl register as fast as possible to test interference 
+ *     with loop 5
+ *
+ *  There are different test blocks which are run:
+ *  -# Single treaded pre-tests (only run one at the beginning)
+ *    - Loop 1 for motor 0 and 1 one a after the other
+ *    - Loop 5 for motor 0 and 1 without delay one after the other
+ *  -# TMC429 tests
+ *    - Loop 1 for motor 0 and 1 in parallel
+ *    - Loop 1 for motor 0 and 1, plus loops 2,3, and 4 in parallel
+ *  -# TMC260 tests
+ *    - Loop 5 with delay and loop 6 for motor 0
+ *    - Loop 5 with delay and loop 6 for motor 1
+ *    - Loop 5 without delay and loop 6 for motor 0
+ *    - Loop 5 without delay and loop 6 for motor 1
+ *  -# Combined test
+ *  -# All loops (loops 1, 5 and 6 for both motors. Loop 5 with delay for motor 0,
+ *     without delay for motor 1)
+ */ 
+
 class SpiReadWriteThreadStressTest{
 public:
   SpiReadWriteThreadStressTest( std::string deviceFileName, std::string mapFileName,
@@ -16,9 +53,14 @@ public:
   void run();
 
 private:  
-  void preTests();
+  // the components of the test block: Loops which run in individual threads
+
+  /// write, read and check the results for the TMC249 controller chip
   void controllerSpiReadWriteTestLoop(unsigned int motorID, unsigned int nLoopsMax = 0);
 
+  
+
+  void preTests();
   void testControllerSpiTwoThreads();
   void printStatus();
   
@@ -28,7 +70,26 @@ private:
   
   unsigned int _nCycles;
 
-  volatile unsigned int _errorsInControllerSpiTwoThreads;
+  // The following block of variables is accessed from multiple threads and protected
+  // by a mutex. Don't access them without locking the mutex!
+  // The counters are volatile because they are changed by the threads. The 
+  // string is not modified from the threads thus does not have to be volatile 
+  // FIXME: The current boost version does not know atomic yet. Change to atomic 
+  // once Ubuntu 12.4 is abandoned and get rid of the error-prone mutex
+  boost::mutex _errorCountingMutex;
+  volatile unsigned int _errorsInThisTest;
+  volatile unsigned int _errorsInThisCycle;
+  volatile unsigned int _errorsTotal;
+  std::string _currentTestName;
+  
+  /// The function name is given to the increaseErrorCount function for logging
+  /// purposes.
+  void increaseErrorCount(std::string functionName);
+
+  // A flag to syncronise multiple threads. As it is a bool which is only read or set
+  // (atomic) it is not protected by a mutex.
+  // FIXME: The current boost version does not know atomic yet. Change to atomic 
+  // once Ubuntu 12.4 is abandoned.
   volatile bool _quitCurrentTest;
 };
 
@@ -38,43 +99,62 @@ SpiReadWriteThreadStressTest::SpiReadWriteThreadStressTest( std::string deviceFi
   : _deviceFileName(deviceFileName), _mapFileName(mapFileName),
     _motorConfigFileName(motorConfigFileName),
     _nCycles(0),
-    _errorsInControllerSpiTwoThreads(0),
+    _errorsInThisTest(0),
+    _errorsInThisCycle(0),
+    _errorsTotal(0),
     _quitCurrentTest(false){
 }
 
 void SpiReadWriteThreadStressTest::preTests(){
   // the that the version without multi-threading works
-  std::cout << "Running single-threaded tests..." << std::endl;
-  controllerSpiReadWriteTestLoop(0, 10);
-  std::cout << "Controller read/write  on motor 0 finished, " << _errorsInControllerSpiTwoThreads
-	    << " errors until now."<< std::endl;
-  _quitCurrentTest=false;
-  controllerSpiReadWriteTestLoop(1, 10);
-  std::cout << "Controller read/write  on motor 1 finished, " << _errorsInControllerSpiTwoThreads
-	    << " errors until now."<< std::endl;
+  std::cout << "Running single threaded pre-tests..." << std::endl;
+  _currentTestName = "Controller read/write  on motor 0";
+  controllerSpiReadWriteTestLoop(0, 1000);
+  printStatus();
+
+  _currentTestName = "Controller read/write  on motor 1";
+  // no need to lock the mutex, not multi-threaded here
+  _errorsInThisTest = 0;
+  controllerSpiReadWriteTestLoop(1, 1000);
+  printStatus();
 }
 
 void SpiReadWriteThreadStressTest::run(){
   preTests();
 
   while (true){
+    std::cout << "Starting cycle " << _nCycles << std::endl;
+    _errorsInThisCycle = 0;
+  
     testControllerSpiTwoThreads();
-    printStatus();
+
+    std::cout << "\nCycle " << _nCycles++ << " finished with "
+	      << _errorsInThisCycle << " errors, " << _errorsTotal
+	      << " errors in total up to now."<< std::endl;
+
     // for development only
-    if (_nCycles==10) break;
+   if (_nCycles==10) break;
   }// while true
 }
 
 void SpiReadWriteThreadStressTest::testControllerSpiTwoThreads(){
 
   _quitCurrentTest=false;
+  _currentTestName = "Controller read/write with two threads";
+  _errorsInThisTest = 0;
 
   // run one instance of in a separate thread...
   boost::thread motor0Thread( boost::bind( &SpiReadWriteThreadStressTest::controllerSpiReadWriteTestLoop, boost::ref(*this), 0, 10000) );
   
+  // run the second instance in this thread
   controllerSpiReadWriteTestLoop(1);
   
+  // this should return immediately because the separate thread triggers the end of
+  // the loop in this thread, so at this point both loops are finished
   motor0Thread.join();
+  
+  // printing the status must only be done after all threads have returned.
+  printStatus();
 }
 
 void SpiReadWriteThreadStressTest::controllerSpiReadWriteTestLoop(unsigned int motorID,
@@ -94,9 +174,7 @@ void SpiReadWriteThreadStressTest::controllerSpiReadWriteTestLoop(unsigned int m
      motorControler->setTargetPosition(i & tmc429::SPI_DATA_MASK);
      if (  (motorControler->getTargetPosition() & tmc429::SPI_DATA_MASK) 
 	   !=  (i & tmc429::SPI_DATA_MASK) ){
-       std::cout << "Error in controllerSpiReadWriteTestLoop of FIXME " 
-		 << std::endl;
-       ++_errorsInControllerSpiTwoThreads;
+       increaseErrorCount("controllerSpiReadWriteTestLoop");
      }
      //     std::cout << "quitCurrentTest = " <<(_quitCurrentTest?"true":"false")  << std::endl;
      if (_quitCurrentTest){
@@ -112,10 +190,23 @@ void SpiReadWriteThreadStressTest::controllerSpiReadWriteTestLoop(unsigned int m
 }
 
 void SpiReadWriteThreadStressTest::printStatus(){
-  std::cout << ++_nCycles << " cycle" ;
-  std::cout << ( _nCycles==1 ? "" : "s ") 
-	    << " finished, " << _errorsInControllerSpiTwoThreads
-	    << " errors detected." << std::endl;;
+  // no need to lock the mutex, this function shall not be called in multi-threaded
+  // contect.
+  std::cout << "  Test \"" << _currentTestName << "\" finished with " 
+	    << _errorsInThisTest << " errors, total errors until now: "
+	    << _errorsTotal << std::endl;
+}
+
+void SpiReadWriteThreadStressTest::increaseErrorCount(std::string functionName){
+  _errorCountingMutex.lock();
+  ++_errorsInThisTest;
+  ++_errorsInThisCycle;
+  ++_errorsTotal;
+  std::cout << "  - Error in loop " << functionName << " of test \""
+	    << _currentTestName << "\", total errors in this test/cycle/total " 
+	    << _errorsInThisTest << "/" << _errorsInThisCycle << "/" 
+	    << _errorsTotal << std::endl;
+  _errorCountingMutex.unlock();
 }
 
 int main(int argc, char* argv []){
