c01322f5b081648dc09d66e2a9b77c3f152270d9
varghese <varghese@5f601a5c-88b9-47f3-b851-818f6741d00d>
2015-08-18 07:22:13 +0000

    Made the following cosmetic changes: - changed #define to const string, for the constants in testConfigConstants.h - changed existing module name: MD220. to MD22_0. - introduced new module MD22_1
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/MotorDriverCard/trunk@1353 5f601a5c-88b9-47f3-b851-818f6741d00d

--- tests/include/testConfigConstants.h
+++ tests/include/testConfigConstants.h
@@ -1,19 +1,17 @@
 #ifndef MTCA4U_TEST_MD22_CONFIG_CONSTANTS_H
 #define MTCA4U_TEST_MD22_CONFIG_CONSTANTS_H
 
-#define MODULE_NAME_0 "MD220"
+const std::string MODULE_NAME_0("MD22_0");
+const std::string MODULE_NAME_1("MD22_1");
 //#define MODULE_NAME_0 ""
 
-#define MAP_FILE_NAME "DFMC_MD22_test.mapp"
-#define BROKEN_MAP_FILE_NAME "DFMC_MD22_broken.mapp"
-#define DUMMY_MOC_MAP "mtcadummy_DFMC_MD22_mock.mapp"
+const std::string MAP_FILE_NAME ("DFMC_MD22_test.mapp");
+const std::string BROKEN_MAP_FILE_NAME("DFMC_MD22_broken.mapp");
+const std::string DUMMY_MOC_MAP("mtcadummy_DFMC_MD22_mock.mapp");
 
-#define CONFIG_FILE "VT21-MotorDriverCardConfig.xml"
+const std::string CONFIG_FILE("VT21-MotorDriverCardConfig.xml");
 
-#define DUMMY_DEV_PATH "/dev/mtcadummys0"
-
-
-
+const std::string DUMMY_DEV_PATH("/dev/mtcadummys0");
 
 
 #endif //MTCA4U_TEST_MD22_CONFIG_CONSTANTS_H
