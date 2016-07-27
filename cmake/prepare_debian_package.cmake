162afeff9d3c77fc6251a27e630dd6be3625d6fd
Martin Killenberg <martin.killenberg@desy.de>
2016-07-27 13:18:28 +0200

    I hope I fixed the debian packaging. Have to commit and tag before I can test it.

--- cmake/prepare_debian_package.cmake
+++ cmake/prepare_debian_package.cmake
@@ -19,6 +19,7 @@ message("mtca4u-deviceaccess_SOVERSION ${mtca4u-deviceaccess_SOVERSION}")
 #Nothing to change, just copy
 file(COPY ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/compat
            ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/rules
+           ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/copyright
 	   ${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/mtca4u-motordrivercard-configcalculator.install
      DESTINATION debian_from_template)
 
@@ -33,14 +34,26 @@ configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-moto
 configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/control.in
                debian_from_template/control @ONLY)
 
-configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/copyright.in
-               debian_from_template/copyright @ONLY)
-
 configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-motordrivercardDEBVERSION.shlib.in
                debian_from_template/libmtca4u-motordrivercard${${PROJECT_NAME}_DEBVERSION}.shlib @ONLY)
 
 configure_file(${CMAKE_SOURCE_DIR}/cmake/debian_package_templates/libmtca4u-motordrivercard-dev.install.in
                debian_from_template/libmtca4u-motordrivercard-dev.install @ONLY)
+
+#For convenience: Also create an install script for DESY
+#The shared library package has the version number in the package name
+set(PACKAGE_BUILDVERSION_ENVIRONMENT_VARIABLE_NAME ${${PROJECT_NAME}_BUILDVERSION_ENVIRONMENT_VARIABLE_NAME})
+set(PACKAGE_BASE_NAME "libmtca4u-motordrivercard")
+set(PACKAGE_FULL_LIBRARY_VERSION "${${PROJECT_NAME}_FULL_LIBRARY_VERSION}")
+set(PACKAGE_GIT_URI "https://github.com/ChimeraTK/MotorDriverCard.git")
+set(PACKAGE_TAG_VERSION ${${PROJECT_NAME}_VERSION})
+set(PACKAGE_MESSAGE " Debian package for MTCA4U MotorDriverCard ${${PROJECT_NAME}_FULL_LIBRARY_VERSION}")
+set(PACKAGE_NAME "${PACKAGE_BASSE_NAME}${${PROJECT_NAME}_DEBVERSION}")
+#The development package does not have the version in the name
+set(PACKAGE_DEV_NAME "libmtca4u-motordrivercard-dev")
+#The binaries are in a separate package because they also do not have a version number in the package name
+set(PACKAGE_BIN_NAME "mtca4u-motordrivercard-configcalculator")
+set(PACKAGE_FILES_WILDCARDS "${PACKAGE_NAME}_*.deb ${PACKAGE_DEV_NAME}_*.deb ${PACKAGE_BIN_NAME}_*.deb libmtca4u-motordrivercard_*.changes")
 
 #Copy and configure the shell script which performs the actual 
 #building of the package
@@ -51,15 +64,6 @@ configure_file(${CMAKE_SOURCE_DIR}/cmake/make_debian_package.sh.in
 #(You could instead run make_debian_package.sh yourself, hm...)
 add_custom_target(debian_package ${CMAKE_BINARY_DIR}/make_debian_package.sh
                   COMMENT Building debian package for tag ${${PROJECT_NAME}_VERSION})
-
-#For convenience: Also create an install script for DESY
-#The shared library package has the version number in the package name
-set(PACKAGE_NAME "libmtca4u-motordrivercard${${PROJECT_NAME}_DEBVERSION}")
-#The development package does not have the version in the name
-set(PACKAGE_DEV_NAME "libmtca4u-motordrivercard-dev")
-#The binaries are in a separate package because they also do not have a version number in the package name
-set(PACKAGE_BIN_NAME "mtca4u-motordrivercard-configcalculator")
-set(PACKAGE_FILES_WILDCARDS "${PACKAGE_NAME}_*.deb ${PACKAGE_DEV_NAME}_*.deb ${PACKAGE_BIN_NAME}_*.deb libmtca4u-motordrivercard_*.changes")
 
 configure_file(${CMAKE_SOURCE_DIR}/cmake/install_debian_package_at_DESY.sh.in
                install_debian_package_at_DESY.sh @ONLY)
