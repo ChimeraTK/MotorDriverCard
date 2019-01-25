f3f38acd652ef48050f917c40b01a8d471ae5969
Christoph Kampmeyer <christoph.kampmeyer@desy.de>
2019-01-25 07:32:00 +0100

    Adapted tests to fit to source code directory structure.

--- tests/src/steppermotor/testStepperMotorChimeraTK.cc
+++ tests/src/steppermotor/testStepperMotorChimeraTK.cc
@@ -13,6 +13,7 @@ using namespace boost::unit_test_framework;
 #include "BasicStepperMotor.h"
 #include "TMC429Constants.h"
 #include "DFMC_MD22Dummy.h"
+#include "MotorDriverCard.h"
 #include "MotorControlerDummy.h"
 #include "MotorDriverCardFactory.h"
 #include "MotorDriverException.h"
@@ -700,148 +701,127 @@ BOOST_AUTO_TEST_CASE( testConverter ){
 }
 
 
-BOOST_AUTO_TEST_CASE( testLogSettings ){
+//FIXME The following tests are not related to the ChimeraTK interface,
+//      also existent in the mtca4u test?
 
-  _stepperMotor->setLogLevel(Logger::NO_LOGGING);
-  BOOST_CHECK( _stepperMotor->getLogLevel() == Logger::NO_LOGGING );
+//BOOST_AUTO_TEST_CASE( testGetSetSpeedLimit ){
 
-  _stepperMotor->setLogLevel(Logger::ERROR);
-  BOOST_CHECK(_stepperMotor->getLogLevel() == Logger::ERROR);
-
-  _stepperMotor->setLogLevel(Logger::WARNING);
-  BOOST_CHECK(_stepperMotor->getLogLevel() == Logger::WARNING);
-
-  _stepperMotor->setLogLevel(Logger::INFO);
-  BOOST_CHECK(_stepperMotor->getLogLevel() == Logger::INFO);
-
-  _stepperMotor->setLogLevel(Logger::DETAIL);
-  BOOST_CHECK(_stepperMotor->getLogLevel() == Logger::DETAIL);
-
-  _stepperMotor->setLogLevel(Logger::FULL_DETAIL);
-  BOOST_CHECK(_stepperMotor->getLogLevel() == Logger::FULL_DETAIL);
-
-  _stepperMotor->setLogLevel(Logger::NO_LOGGING);
-}
+//  // setup
+//  auto dmapFile = ChimeraTK::getDMapFilePath();
+//  ChimeraTK::setDMapFilePath("./dummies.dmap");
+//  auto dummyModeStatus = mtca4u::MotorDriverCardFactory::instance().getDummyMode();
+//  mtca4u::MotorDriverCardFactory::instance().setDummyMode(false);
+//  mtca4u::StepperMotor motor("DFMC_MD22_PERSISTENT_BACKEND", "MD22_0", 0, "custom_speed_and_curruent_limits.xml");
 
 
-BOOST_AUTO_TEST_CASE( testGetSetSpeedLimit ){
+//  // Expected max speed  capability in this case:[from custom_speed_and_curruent_limits.xml]
+//  //   Vmax = 1398 (0x576)
+//  //   fclk = 32000000
+//  //   pulse_div = 6
+//  // expectedMaxSpeedLimitInUstepsPerSecond = Vmax * fclk/((2^pulse_div) * 2048 * 32)
+//  //
+//  auto expectedMaxSpeedLimit = (1398.0 * 32000000.0) / (exp2(6) * 2048 * 32);
 
-  // setup
-  auto dmapFile = ChimeraTK::getDMapFilePath();
-  ChimeraTK::setDMapFilePath("./dummies.dmap");
-  auto dummyModeStatus = mtca4u::MotorDriverCardFactory::instance().getDummyMode();
-  mtca4u::MotorDriverCardFactory::instance().setDummyMode(false);
-  mtca4u::StepperMotor motor("DFMC_MD22_PERSISTENT_BACKEND", "MD22_0", 0, "custom_speed_and_curruent_limits.xml");
+//  // expectedMinimumSpeedLimitInUstepsPerSec = Vmin * fclk/((2^pulse_div) * 2048 * 32)
+//  auto expectedMinimumSpeed = (1.0 * 32000000.0) / // VMin = 1 (1.0)
+//                              (exp2(6) * 2048 * 32);
 
+//  //verify getMaxSpeedCapability
+//  BOOST_CHECK(motor.getMaxSpeedCapability() == expectedMaxSpeedLimit);
 
-  // Expected max speed  capability in this case:[from custom_speed_and_curruent_limits.xml]
-  //   Vmax = 1398 (0x576)
-  //   fclk = 32000000
-  //   pulse_div = 6
-  // expectedMaxSpeedLimitInUstepsPerSecond = Vmax * fclk/((2^pulse_div) * 2048 * 32)
-  //
-  auto expectedMaxSpeedLimit = (1398.0 * 32000000.0) / (exp2(6) * 2048 * 32);
+//  // set speed > MaxAllowedSpeed
+//  BOOST_CHECK(motor.setUserSpeedLimit(expectedMaxSpeedLimit) == expectedMaxSpeedLimit);
+//  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMaxSpeedLimit);
+//  BOOST_CHECK(motor.setUserSpeedLimit(expectedMaxSpeedLimit + 10) == expectedMaxSpeedLimit);
+//  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMaxSpeedLimit);
 
-  // expectedMinimumSpeedLimitInUstepsPerSec = Vmin * fclk/((2^pulse_div) * 2048 * 32)
-  auto expectedMinimumSpeed = (1.0 * 32000000.0) / // VMin = 1 (1.0)
-                              (exp2(6) * 2048 * 32);
+//  // set speed < MinAllowedSpeed
+//  BOOST_CHECK(motor.setUserSpeedLimit(expectedMinimumSpeed) == expectedMinimumSpeed);
+//  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMinimumSpeed);
+//  BOOST_CHECK(motor.setUserSpeedLimit(expectedMinimumSpeed - 10) == expectedMinimumSpeed);
+//  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMinimumSpeed);
+//  BOOST_CHECK(motor.setUserSpeedLimit(-100) == expectedMinimumSpeed);
+//  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMinimumSpeed);
 
-  //verify getMaxSpeedCapability
-  BOOST_CHECK(motor.getMaxSpeedCapability() == expectedMaxSpeedLimit);
+//  // set speed in valid range [Vmin, Vmax]
+//  // speed = 8000 microsteps per second -> results in Vmax = 1048.576
+//  // Vmax 1048.576  floors to 1048 => which gives returned/actual set speed as
+//  // 7995.6054... microsteps per second
+//  auto expectedSetSpeed = (1048.0 * 32000000.0) / (exp2(6) * 2048 * 32);
+//  BOOST_CHECK(motor.setUserSpeedLimit(8000) == expectedSetSpeed);
+//  BOOST_CHECK(motor.getUserSpeedLimit() == expectedSetSpeed);
 
-  // set speed > MaxAllowedSpeed
-  BOOST_CHECK(motor.setUserSpeedLimit(expectedMaxSpeedLimit) == expectedMaxSpeedLimit);
-  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMaxSpeedLimit);
-  BOOST_CHECK(motor.setUserSpeedLimit(expectedMaxSpeedLimit + 10) == expectedMaxSpeedLimit);
-  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMaxSpeedLimit);
+//  // Verify that Vmax is 1048 in the internal register (which is the
+//  // VMax corresponding to expectedSetSpeed)
+//  auto md22_instance = boost::dynamic_pointer_cast<mtca4u::DFMC_MD22Dummy>(
+//      BackendFactory::getInstance().createBackend(
+//          "DFMC_MD22_PERSISTENT_BACKEND"));
+//  BOOST_ASSERT(md22_instance != NULL);
+//  mtca4u::TMC429OutputWord vMaxRegister = readDFMCDummyMotor0VMaxRegister(md22_instance);
+// BOOST_CHECK(vMaxRegister.getDATA() == 1048);
 
-  // set speed < MinAllowedSpeed
-  BOOST_CHECK(motor.setUserSpeedLimit(expectedMinimumSpeed) == expectedMinimumSpeed);
-  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMinimumSpeed);
-  BOOST_CHECK(motor.setUserSpeedLimit(expectedMinimumSpeed - 10) == expectedMinimumSpeed);
-  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMinimumSpeed);
-  BOOST_CHECK(motor.setUserSpeedLimit(-100) == expectedMinimumSpeed);
-  BOOST_CHECK(motor.getUserSpeedLimit() == expectedMinimumSpeed);
-
-  // set speed in valid range [Vmin, Vmax]
-  // speed = 8000 microsteps per second -> results in Vmax = 1048.576
-  // Vmax 1048.576  floors to 1048 => which gives returned/actual set speed as
-  // 7995.6054... microsteps per second
-  auto expectedSetSpeed = (1048.0 * 32000000.0) / (exp2(6) * 2048 * 32);
-  BOOST_CHECK(motor.setUserSpeedLimit(8000) == expectedSetSpeed);
-  BOOST_CHECK(motor.getUserSpeedLimit() == expectedSetSpeed);
-
-  // Verify that Vmax is 1048 in the internal register (which is the
-  // VMax corresponding to expectedSetSpeed)
-  auto md22_instance = boost::dynamic_pointer_cast<mtca4u::DFMC_MD22Dummy>(
-      BackendFactory::getInstance().createBackend(
-          "DFMC_MD22_PERSISTENT_BACKEND"));
-  BOOST_ASSERT(md22_instance != NULL);
-  mtca4u::TMC429OutputWord vMaxRegister = readDFMCDummyMotor0VMaxRegister(md22_instance);
- BOOST_CHECK(vMaxRegister.getDATA() == 1048);
-
-  //teardown
-    mtca4u::MotorDriverCardFactory::instance().setDummyMode(dummyModeStatus);
-    ChimeraTK::setDMapFilePath(dmapFile);
-}
+//  //teardown
+//    mtca4u::MotorDriverCardFactory::instance().setDummyMode(dummyModeStatus);
+//    ChimeraTK::setDMapFilePath(dmapFile);
+//}
 
 
-BOOST_AUTO_TEST_CASE( testGetSetCurrent ){
+//BOOST_AUTO_TEST_CASE( testGetSetCurrent ){
 
-  auto dmapFile = ChimeraTK::getDMapFilePath();
-  ChimeraTK::setDMapFilePath("./dummies.dmap");
-  auto dummyModeStatus = mtca4u::MotorDriverCardFactory::instance().getDummyMode();
-  mtca4u::MotorDriverCardFactory::instance().setDummyMode(false);
-  mtca4u::StepperMotor motor("DFMC_MD22_PERSISTENT_BACKEND", "MD22_0", 0, "custom_speed_and_curruent_limits.xml");
+//  auto dmapFile = ChimeraTK::getDMapFilePath();
+//  ChimeraTK::setDMapFilePath("./dummies.dmap");
+//  auto dummyModeStatus = mtca4u::MotorDriverCardFactory::instance().getDummyMode();
+//  mtca4u::MotorDriverCardFactory::instance().setDummyMode(false);
+//  mtca4u::StepperMotor motor("DFMC_MD22_PERSISTENT_BACKEND", "MD22_0", 0, "custom_speed_and_curruent_limits.xml");
 
-  // The motor has been configured for a maximum current of .24 Amps, with 1.8A
-  // being the maximum the driver ic can provide. This translates to a current
-  // scale according to the formula:
-  // (.24 * 32)/1.8 - 1 == 3.266
-  // the 32 in the above formula comes from the 32 current scale levels for the
-  // tmc260.
-  // The tmc260 current scale register takes a whole number. So floor(3.266)
-  // goes into the current scale register. Because of this we get an effective
-  // current limit of:
-  // 1.8 * (3 + 1)/32 == .225 A
+//  // The motor has been configured for a maximum current of .24 Amps, with 1.8A
+//  // being the maximum the driver ic can provide. This translates to a current
+//  // scale according to the formula:
+//  // (.24 * 32)/1.8 - 1 == 3.266
+//  // the 32 in the above formula comes from the 32 current scale levels for the
+//  // tmc260.
+//  // The tmc260 current scale register takes a whole number. So floor(3.266)
+//  // goes into the current scale register. Because of this we get an effective
+//  // current limit of:
+//  // 1.8 * (3 + 1)/32 == .225 A
 
-  auto expectedSafeCurrentLimitValue = 1.8 * (4.0)/32.0;
-  auto safeCurrentLimit = motor.getSafeCurrentLimit();
-  BOOST_CHECK(safeCurrentLimit == expectedSafeCurrentLimitValue);
+//  auto expectedSafeCurrentLimitValue = 1.8 * (4.0)/32.0;
+//  auto safeCurrentLimit = motor.getSafeCurrentLimit();
+//  BOOST_CHECK(safeCurrentLimit == expectedSafeCurrentLimitValue);
 
-  // set Current to Beyond the safe limit
-  BOOST_CHECK(motor.setUserCurrentLimit(safeCurrentLimit + 10) == safeCurrentLimit);
-  BOOST_CHECK(motor.getUserCurrentLimit() == safeCurrentLimit);
-  BOOST_CHECK(motor.setUserCurrentLimit(safeCurrentLimit) == safeCurrentLimit);
-  BOOST_CHECK(motor.getUserCurrentLimit() == safeCurrentLimit);
+//  // set Current to Beyond the safe limit
+//  BOOST_CHECK(motor.setUserCurrentLimit(safeCurrentLimit + 10) == safeCurrentLimit);
+//  BOOST_CHECK(motor.getUserCurrentLimit() == safeCurrentLimit);
+//  BOOST_CHECK(motor.setUserCurrentLimit(safeCurrentLimit) == safeCurrentLimit);
+//  BOOST_CHECK(motor.getUserCurrentLimit() == safeCurrentLimit);
 
-  // set a current below minimum value.
-  // Minimum value of current (when current scale is 0):
-  // 1.8 * (0 + 1)/32 == .05625A
-  auto minimumCurrent = 1.8 * 1.0/32.0;
-  BOOST_CHECK(motor.setUserCurrentLimit(0) == minimumCurrent);
-  BOOST_CHECK(motor.getUserCurrentLimit() == minimumCurrent);
-  BOOST_CHECK(motor.setUserCurrentLimit(-10) == minimumCurrent);
-  BOOST_CHECK(motor.getUserCurrentLimit() == minimumCurrent);
+//  // set a current below minimum value.
+//  // Minimum value of current (when current scale is 0):
+//  // 1.8 * (0 + 1)/32 == .05625A
+//  auto minimumCurrent = 1.8 * 1.0/32.0;
+//  BOOST_CHECK(motor.setUserCurrentLimit(0) == minimumCurrent);
+//  BOOST_CHECK(motor.getUserCurrentLimit() == minimumCurrent);
+//  BOOST_CHECK(motor.setUserCurrentLimit(-10) == minimumCurrent);
+//  BOOST_CHECK(motor.getUserCurrentLimit() == minimumCurrent);
 
-  // Current in a valid range: 0 - .24A
-  // I = .2A
-  // CS = (.20 * 32)/1.8 - 1 = floor(2.556) = 2
-  // For CS 2: Expected current = (1.8 * (2 + 1))/32 = .16875A
-  auto expectedCurrent = (1.8 * (2 + 1))/32.0;
-  BOOST_CHECK(motor.setUserCurrentLimit(.2) == expectedCurrent);
-  BOOST_CHECK(motor.getUserCurrentLimit() == expectedCurrent);
-  // Verify that CS is 2 in the internal register ()
-  auto md22_instance = boost::dynamic_pointer_cast<mtca4u::DFMC_MD22Dummy>(
-      BackendFactory::getInstance().createBackend("DFMC_MD22_PERSISTENT_BACKEND"));
-  BOOST_ASSERT(md22_instance != nullptr);
-  mtca4u::StallGuardControlData stallGuard = readDFMCDummyMotor0CurrentScale(md22_instance);
-  BOOST_CHECK(stallGuard.getCurrentScale() == 2);
+//  // Current in a valid range: 0 - .24A
+//  // I = .2A
+//  // CS = (.20 * 32)/1.8 - 1 = floor(2.556) = 2
+//  // For CS 2: Expected current = (1.8 * (2 + 1))/32 = .16875A
+//  auto expectedCurrent = (1.8 * (2 + 1))/32.0;
+//  BOOST_CHECK(motor.setUserCurrentLimit(.2) == expectedCurrent);
+//  BOOST_CHECK(motor.getUserCurrentLimit() == expectedCurrent);
+//  // Verify that CS is 2 in the internal register ()
+//  auto md22_instance = boost::dynamic_pointer_cast<mtca4u::DFMC_MD22Dummy>(
+//      BackendFactory::getInstance().createBackend("DFMC_MD22_PERSISTENT_BACKEND"));
+//  BOOST_ASSERT(md22_instance != nullptr);
+//  mtca4u::StallGuardControlData stallGuard = readDFMCDummyMotor0CurrentScale(md22_instance);
+//  BOOST_CHECK(stallGuard.getCurrentScale() == 2);
 
-  //teardown
-    mtca4u::MotorDriverCardFactory::instance().setDummyMode(dummyModeStatus);
-    ChimeraTK::setDMapFilePath(dmapFile);
-}
+//  //teardown
+//    mtca4u::MotorDriverCardFactory::instance().setDummyMode(dummyModeStatus);
+//    ChimeraTK::setDMapFilePath(dmapFile);
+//}
 
 BOOST_AUTO_TEST_SUITE_END()
 
