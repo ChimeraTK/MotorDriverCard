f3a4bf555008e565efff01b1073099ef1ff56688
Jens Georg <jens.georg@desy.de>
2021-12-06 17:01:01 +0100

    tests: Switch some tests to new map file

--- tests/include/testConfigConstants.h
+++ tests/include/testConfigConstants.h
@@ -4,7 +4,7 @@
 const std::string MODULE_NAME_0("MD22_0");
 const std::string MODULE_NAME_1("MD22_1");
 
-const std::string MAP_FILE_NAME("DFMC_MD22_test.mapp");
+const std::string MAP_FILE_NAME("newer_firmware_mapfile.map");
 const std::string DFMC_ALIAS("DFMC_MD22");
 const std::string DFMC_ALIAS2("DFMC_MD22_PERSISTENT_BACKEND");
 const std::string BROKEN_MAP_FILE_NAME("DFMC_MD22_broken.mapp");
