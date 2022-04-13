fa3a7fe2d2428b3bdb20517730ed2c53abecd974
Jens Georg <jens.georg@desy.de>
2022-04-13 09:28:41 +0200

    Make it possible to close the underlying device
    Change the MotorCardFactory cache to use weak pointers so it is possible
    to close the device by giving up all references to the card, for
    recovering on error situations

--- backend/src/MotorDriverCardFactory.cc
+++ backend/src/MotorDriverCardFactory.cc
@@ -24,28 +24,31 @@ namespace mtca4u {
       std::string mapModuleName,
       std::string motorConfigFileName) {
     boost::lock_guard<boost::mutex> guard(_factoryMutex);
-    // if the card is not in the map it will be created, otherwise the
-    // existing one is delivered
-    // TSK - fix me - now key must be a pair of deviceFileName and mapModuleName,
-    // not only deviceFileName
-    boost::shared_ptr<MotorDriverCard>& motorDriverCard = _motorDriverCards[std::make_pair(alias, mapModuleName)];
 
-    // create a new instance of the motor driver card if the shared pointer is
-    // empty, which means it has just been added to the map
-    if(!motorDriverCard) {
-      if(_dummyMode) { // if (deviceFileName=="custom://MotorDriverCardDummy")
-        // just create a MotorDriverCardDummy
-        motorDriverCard.reset(new MotorDriverCardDummy());
-      }
-      else {
-        boost::shared_ptr<ChimeraTK::Device> device(new ChimeraTK::Device);
-        device->open(alias);
-        MotorDriverCardConfig cardConfig = MotorDriverCardConfigXML::read(motorConfigFileName);
 
-        motorDriverCard.reset(new MotorDriverCardImpl(device, mapModuleName, cardConfig));
-      } // else deviceFileName=="/dummy/MotorDriverCard"
+    // Check if we have the card and if the weak reference is still valid. That way we can guarantee that there is only
+    // one MotorDriverCard instance accessing the device (at least in this process) while also being able to re-open
+    // the underlying device by giving up all motors using the card.
+    auto id = std::make_pair(alias, mapModuleName);
+    auto iterator = _motorDriverCards.find(id);
+    if(iterator != _motorDriverCards.end()) {
+      auto card = iterator->second.lock();
+      if(card) return card;
+    }
 
-    } // if (!motorDriverCard)
+    boost::shared_ptr<MotorDriverCard> motorDriverCard;
+    if(_dummyMode) {
+      // just create a MotorDriverCardDummy
+      motorDriverCard.reset(new MotorDriverCardDummy());
+    }
+    else {
+      boost::shared_ptr<ChimeraTK::Device> device(new ChimeraTK::Device);
+      device->open(alias);
+      MotorDriverCardConfig cardConfig = MotorDriverCardConfigXML::read(motorConfigFileName);
+
+      motorDriverCard.reset(new MotorDriverCardImpl(device, mapModuleName, cardConfig));
+    }
+    _motorDriverCards[id] = motorDriverCard;
 
     return motorDriverCard;
   }
