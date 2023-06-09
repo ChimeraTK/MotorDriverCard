db00b027352d7d41cd4968c8489859200696fac0
Jens Georg <jens.georg@desy.de>
2023-06-09 12:54:26 +0200

    Add configuration for end switch polarity
    Implements #8529

--- ConfigCalculator/GUI/QtMotorConfigCalculator.h
+++ ConfigCalculator/GUI/QtMotorConfigCalculator.h
@@ -1,41 +1,42 @@
 #ifndef QT_MOTOR_CONFIG_CALCULATOR_H
 #define QT_MOTOR_CONFIG_CALCULATOR_H
 
-#include "MotorConfigWidget.h"
 #include <QPushButton>
 #include <QScrollArea>
 #include <QTabWidget>
+#include "MotorConfigWidget.h"
 
 #include "ParametersPanel.h"
 
 /** A widget containing two MotorConfigWidget in a TabWidget,
  *  and a write button which write the config to an XML file\
  */
-class QtMotorConfigCalculator : public QWidget {
-  Q_OBJECT
+class QtMotorConfigCalculator : public QWidget
+{
+    Q_OBJECT
 
- public:
-  /** The constructor expects no parameters except for the
+public:
+    /** The constructor expects no parameters except for the
    *  parent widget.
    */
-  QtMotorConfigCalculator(QWidget* parent_ = NULL);
-  QtMotorConfigCalculator(QtMotorConfigCalculator const&) = delete;
-  QtMotorConfigCalculator& operator=(QtMotorConfigCalculator const&) = delete;
+    QtMotorConfigCalculator(QWidget *parent_ = NULL);
+    QtMotorConfigCalculator(QtMotorConfigCalculator const &) = delete;
+    QtMotorConfigCalculator &operator=(QtMotorConfigCalculator const &) = delete;
 
- private:
-  QVBoxLayout* verticalLayout;
-  QTabWidget* motorTabWidget;
-  QPushButton* writeButton;
+private:
+    QVBoxLayout *verticalLayout;
+    QTabWidget *motorTabWidget;
+    QPushButton *writeButton;
 
-  QTabWidget* expertTabWidget;
-  std::vector<MotorConfigWidget*> motorConfigWidgets;
-  std::vector<ParametersPanel*> motorExpertWidgets;
-  ParametersPanel* cardExpertWidget;
+    QTabWidget *expertTabWidget;
+    std::vector<MotorConfigWidget *> motorConfigWidgets;
+    std::vector<ParametersPanel *> motorExpertWidgets;
+    ParametersPanel *cardExpertWidget;
 
- private slots:
-  /** Get the file name via dialogue and perform the writing.
-   */
-  void write();
+private slots:
+    // Get the file name via dialogue and perform the writing.
+    void write();
+    void updateCardExpertPanel(int index);
 };
 
 #endif // QT_MOTOR_CONFIG_CALCULATOR_H
